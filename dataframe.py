a899c609b12e77fe804fc88ec3da778f3d4bcd68
Ivona <cgshony@gmail.com>
2025-08-23 18:19:41 +0300

    implement schema

--- dataframe.py
+++ dataframe.py
@@ -8,12 +8,13 @@ from datetime import datetime
 class DataFrame:
     """TODO:"""
 
-    def __init__(self, column_content):
+    def __init__(self, column_content, schema=None):
         """Validate input data and initialize the Data frame."""
         for column in column_content.values():
             self.is_valid_column(column)
         self._validate_input(column_content)
         self.column_content = column_content
+        self.schema.validate(self.column_content)
 
     @classmethod
     def from_rows(cls, rows):
@@ -65,6 +66,19 @@ class DataFrame:
         """Enable setting a new column."""
         self.validate_column
         self.column_content[name] = value
+
+    def __getattr__(self,name):
+        """Get column by attribute access."""
+        return self.column_content[name]
+    
+    def __setattr__(self, name, value):
+        """Set column via attribute access"""
+        if name != 'column definitions' and name in self.column_content:
+            self.is_valid_column(value)
+            self.column_content[name] = value
+        else:
+            super().__setattr__(name, value)
+
 
     def __str__(self):
         """Represent DataFrame as a string with it's size and contents."""
@@ -133,13 +147,32 @@ class LazyFrame(DataFrame):
         table.add_rows(row for row in zip(*self.column_content.values()))        
         return f"LazyFrame ({self.shape[0]}x{self.shape[1]})"
 
-lf = LazyFrame({"name": ["Гошо", "Тошо", "Пешо"], "age": [25, 35, 30], "height": [180, 140, 220]})
-lf = (lf.filter(lambda row: row["age"] >= 30)
-    .map_column("height", lambda x: x + 10)
-    .filter(lambda row: row["height"] >= 230))
+# lf = LazyFrame({"name": ["Гошо", "Тошо", "Пешо"], "age": [25, 35, 30], "height": [180, 140, 220]})
+# lf = (lf.filter(lambda row: row["age"] >= 30)
+#     .map_column("height", lambda x: x + 10)
+#     .filter(lambda row: row["height"] >= 230))
 
-print(lf.collect())
+# print(lf.collect())
 
+class SchemaError(Exception):
+    """Raised when a schema validation error occurs"""
+
+class Schema:
+    """Schema for validating data types for DataFrames"""
+    def __init__(self, **kwargs):
+        self._schema = kwargs
+
+    def validate(self, column_contents):
+         """Validate column definitions against schema expectations"""
+         for column, column_type in self._schema.items():
+             if column not in column_contents:
+                 raise SchemaError(f"Column {column} must be present according to the schema.")
+             if not isinstance(column_contents[column][0], column_type):
+                 raise SchemaError(f"Typo for column{column} must be {column_type} according to the schema.")
+    
+df = DataFrame({"name": ["Гошо", "Тошо", "Пешо"], "age": [25, 35, 30], "height": [180, 140, 220]},
+               schema=Schema(name=str, age=int, height=int))
+print(df)
 
 @validate_column_types(date=datetime)
 def extract_time_interval(df):
